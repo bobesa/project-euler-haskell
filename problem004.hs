8bd796bc7fa4ec9639edc52ec6726238e6647103
Vlastimil Senfeld <bobesa@gammosaur.com>
2014-07-16 22:43:31 +0200

    ++Problem 004

---
+++ problem004.hs
@@ -0,0 +1,14 @@
+-- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+-- Find the largest palindrome made from the product of two 3-digit numbers.
+
+bottomLimit = 100
+topLimit = 1000
+
+isPalindromic num = (show num) == reverse (show num)
+
+topPalindrome n1 n2 best = if (n1 == topLimit && n2 == topLimit) then best else (inc n1 n2 best)
+	where
+		inc n1 n2 best = if n1 < topLimit then (process (n1 + 1) n2 best) else (process bottomLimit (n2 + 1) best)
+		process n1 n2 best = topPalindrome n1 n2 (if (isPalindromic (n1 * n2)) then (max best (n1 * n2)) else best)
+
+main = print (topPalindrome bottomLimit bottomLimit 0)
