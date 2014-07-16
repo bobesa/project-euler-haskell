7ce11743afec3d02edbc707c13b945b69d27a455
Vlastimil Senfeld <bobesa@gammosaur.com>
2014-07-16 17:02:27 +0200

    Initial commit

---
+++ README.mk
@@ -0,0 +1,4 @@
+Project Euler (Haskell solutions)
+####
+
+Long time ago I was using Haskell language on school and I had fun with it. This is a perfect opportunity to get back into Haskell again. So far it seem like a language unknown to me after these years.
