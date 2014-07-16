24ada6ca7edb1b11234d8e4deeba362eb7a0cbd0
Vlastimil Senfeld <bobesa@gammosaur.com>
2014-07-17 00:12:51 +0200

    array comprehension

--- problem002.hs
+++ problem002.hs
@@ -4,6 +4,14 @@
 
 limit = 4000000
 
-main = print (evenfib 1 1 0)
+-- Way #1: Functionalist
+mainOld = print (evenfib 1 1 0)
 	where 
 		evenfib n1 n2 total = if n2 >= limit then total else (evenfib n2 (n1 + n2) (if (n1 + n2) `mod` 2 == 0 then total + (n1 + n2) else total))
+
+-- Way #2: Array comprehension trick
+-- We will test only first 100 fibonacci numbers
+main = print (sum (filter above (take 100 fibs)))
+	where
+		above num = num < limit && even num 
+		fibs = 1:2:[a + b | (a,b) <- zip fibs (tail fibs)]
